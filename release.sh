2d395c682bc660c58d7c0cafc1cd43a7537d4eba
Nicolas Mailloux <nicolecrivain@gmail.com>
2022-04-11 12:06:06 -0400

    Don't require root anymore when creating SquashFS

--- release.sh
+++ release.sh
@@ -5,19 +5,13 @@ if [ -z "${GITDIR}" ]; then
 	exit 1
 fi
 
-if [ ${EUID} != 0 ]; then
-	echo "This script must be run as root."
-	exit 1
-fi
-
 cd "${GITDIR}"
 git rev-parse HEAD > ./.commit
 chmod u+s "${GITDIR}/bin/busybox"
 chmod u+s "${GITDIR}/bin/busybox-initrd"
 find . -type f -name ".keep" -exec rm {} \;
 rm -f ../recoveryfs.squashfs
-mksquashfs . ../recoveryfs.squashfs -b 1048576 -comp xz -Xdict-size 100% -always-use-fragments -e .git -e release.sh
+mksquashfs . ../recoveryfs.squashfs -b 1048576 -comp xz -Xdict-size 100% -always-use-fragments -all-root -e .git -e release.sh
 rm ./.commit
 find . -type d ! -path "*.git*" -empty -exec touch '{}'/.keep \;
-echo "Root filesystem has been compressed."
-
+echo "Recovery filesystem has been compressed."
