e4681a0d14332121b974c43f34332d9c5028d2ef
Nicolas Mailloux <nicolecrivain@gmail.com>
2023-07-25 08:45:26 -0400

    N306C support

--- opt/recovery/scripts/update.sh
+++ opt/recovery/scripts/update.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+DEVICE="$(cat /opt/device)"
+[ "$(uname -r | grep -o '[^-]*$')" == "n306c" ] && DEVICE_VARIANT="n306c"
+
 mkdir /tmp/update_storage
 mount -t tmpfs tmpfs -o size=140M /tmp/update_storage
 cd /tmp/update_storage && busybox-initrd tar -xf /opt/recovery/update/update_storage.tar.gz
@@ -11,8 +14,10 @@ if [ "${DEVICE}" != "n873" ] && [ "${DEVICE}" != "n236" ] && [ "${DEVICE}" != "n
 	insmod /modules/arcotg_udc.ko
 fi
 if [ "${DEVICE}" == "n306" ] || [ "${DEVICE}" == "n873" ]; then
-	insmod /modules/fs/configfs/configfs.ko
-	insmod /modules/drivers/usb/gadget/libcomposite.ko
-	insmod /modules/drivers/usb/gadget/function/usb_f_mass_storage.ko
+	if [ "${DEVICE_VARIANT}" != "n306c" ]; then
+		insmod /modules/fs/configfs/configfs.ko
+		insmod /modules/drivers/usb/gadget/libcomposite.ko
+		insmod /modules/drivers/usb/gadget/function/usb_f_mass_storage.ko
+	fi
 fi
 insmod /modules/g_mass_storage.ko file=/tmp/update_storage/update_storage removable=y stall=0
